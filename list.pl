79f3a13457412d517ebea3d0dbe74f5cd1a3ae70
Giulio  Jiang <giulio.jiang14@imperial.ac.uk>
2016-01-15 13:28:11 +0000

    done arbpairs

--- list.pl
+++ list.pl
@@ -1,5 +1,5 @@
 /*
-[1] ALL_MEMBERS
+[1] ALL_MEMBERS ..............................................................
 */
 
 all_members([], _Ys).
@@ -8,7 +8,7 @@ all_members([X|Xs], Ys) :-
     all_members(Xs, Ys).
     
 /*
-[2] PAIRS
+[2] PAIRS ....................................................................
 */
 
 pairs([],[]).
@@ -16,3 +16,14 @@ pairs([X|Xs], [(Prev,Succ)|Zs]) :-
     pairs(Xs, Zs),
     Prev is X-1,
     Succ is X+1.
+    
+/*
+[3] ARBPAIRS .................................................................
+*/
+
+arbpairs([],[]).
+arbpairs([X|Xs], [(X,Y)|Ys]) :-
+    arbpairs(Xs, Ys),
+    Y is 2*X.
+arbpairs([X|Xs], [(X,X)|Ys]) :-
+    arbpairs(Xs, Ys).
